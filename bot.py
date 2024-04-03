edbca44233ae1b34dd1f472713d0b44e7737f471
Mikahil Ponomarev <mponomarev23@mail.ru>
2024-04-03 23:08:24 +0300

    migrate to aiogram 3

--- bot.py
+++ bot.py
@@ -1,140 +1,176 @@
 import os
 import re
 import aiohttp
+import asyncio
 from dotenv import load_dotenv
-from aiogram import Bot, Dispatcher, executor, types
-from aiogram.dispatcher import FSMContext
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.dispatcher.filters.state import StatesGroup, State
+from aiogram import F
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command, StateFilter
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.types import Message
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
 
 config = load_dotenv('.env')
 
-storage = MemoryStorage()
 bot = Bot(token=os.getenv('API_TOKEN'))
-dp = Dispatcher(bot, storage=storage)
+
+storage = MemoryStorage()
+dp = Dispatcher(storage=storage)
+
+banks = ['ВТБ', 'АЛЬФА-БАНК', 'РОСБАНК', 'РОССЕЛЬХОЗБАНК', 'ГАЗПРОМБАНК', 'АК БАРС', 'УРАЛСИБ БАНК']
 
 
 class PredictBatchSG(StatesGroup):
-    file = State()
+    batch = State()
+
 
 class RateSG(StatesGroup):
     rate = State()
 
 
 def cancel():
-    return ReplyKeyboardMarkup(resize_keyboard=True).add(KeyboardButton('/cancel'))
+    kb = [
+        [
+            KeyboardButton(text='Отмена'),
+        ],
+    ]
+    return ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True)
 
 
-@dp.message_handler(commands=['cancel'], state='*')
-async def cancel_cmd(message: types.Message, state: FSMContext):
+@dp.message(F.text, Command('cancel'))
+@dp.message(F.text.lower() == "отмена")
+async def cmd_cancel(message: Message, state: FSMContext):
     await message.answer("Отмена", reply_markup=ReplyKeyboardRemove())
-    await state.finish()
+    await state.clear()
 
 
-@dp.message_handler(commands=['start'])
-async def process_start_command(message: types.Message, state, raw_state):
+@dp.message(F.text, Command('start'))
+async def cmd_start(message: Message):
     await message.reply("Готов к работе!")
 
 
-@dp.message_handler(commands=['help'])
-async def process_help_command(message: types.Message):
-    await message.reply(\
+@dp.message(F.text, Command('help'))
+async def cmd_help(message: Message):
+    await message.reply(
         "Список доступных комманд:\n\n"
-        "/predict [lat], [long] - предсказание по координатам\n"
+        "/banks - список доступных банков\n"
+        "/predict [lat], [long], [bank] - предсказание по координатам\n"
         "/predict_batch - предсказания по нескольким парам координат\n"
         "/history - история запросов\n"
         "/rate - оставить обратную связь\n\n"
         "Пример:\n"
-        "/predict 60.56805, 45.04305")
+        "/predict 60.56805, 45.04305, ВТБ")
 
 
-@dp.message_handler(commands=['history'])
-async def process_history_command(message: types.Message):
-    user_id = int(message.from_id)
+@dp.message(F.text, Command('banks'))
+async def cmd_banks(message: Message):
+    await message.reply("Список доступных банков:\n\n"
+                        "ВТБ\n"
+                        "АЛЬФА-БАНК\n"
+                        "РОСБАНК\n"
+                        "РОССЕЛЬХОЗБАНК\n"
+                        "ГАЗПРОМБАНК\n"
+                        "АК БАРС\n"
+                        "УРАЛСИБ БАНК\n")
+
+
+@dp.message(F.text, Command('history'))
+async def cmd_history(message: Message):
+    user_id = int(message.from_user.id)
     async with aiohttp.ClientSession() as session:
-        async with session.get(f'https://atm-project.onrender.com/history/{user_id}') as res:
+        async with session.get(f'https://atmapi-dahy.onrender.com/history/{user_id}') as res:
             history = await res.json()
 
     if len(history) != 0:
-        msg = '\n'.join([f"({pred['lat']}, {pred['long']}): {pred['prediction']}" for pred in history])
+        msg = '\n'.join([f"({pred['lat']}, {pred['long']}, {pred['atm_group']}): {pred['prediction']}" for pred in history])
         await message.answer(msg)
     else:
         await message.answer('Нет истории')
 
 
-@dp.message_handler(commands=['rate'])
-async def process_rate_command(message: types.Message):
+@dp.message(StateFilter(None), F.text, Command('rate'))
+async def cmd_rate(message: Message, state: FSMContext):
     await message.reply("Оставьте отзыв", reply_markup=cancel())
-    await RateSG.rate.set()
+    await state.set_state(RateSG.rate)
 
-    
-@dp.message_handler(state=RateSG.rate)
-async def process_rate_text(message: types.Message,  state: FSMContext):
-    user_id = int(message.from_id)
+
+@dp.message(F.text, RateSG.rate)
+async def rate_text(message: Message,  state: FSMContext):
+    user_id = int(message.from_user.id)
     text = message.text
     async with aiohttp.ClientSession() as session:
-        async with session.post('https://atm-project.onrender.com/feedback',
+        async with session.post('https://atmapi-dahy.onrender.com/feedback',
                                 json={'id_user': user_id,
                                       'feedback': text}):
             pass
-    
-    await message.reply("Спасибо!")
-    await state.finish()
+
+    await message.reply("Спасибо!", reply_markup=ReplyKeyboardRemove())
+    await state.clear()
 
 
-@dp.message_handler(commands=['predict'])
-async def process_predict_command(message: types.Message):
-    user_id = int(message.from_id)
-
-    coordinates = re.split('[^\w.]+', message.text.removeprefix('/predict').strip())
+@dp.message(F.text, Command('predict'))
+async def cmd_predict(message: Message):
+    user_id = int(message.from_user.id)
 
     try:
-        lat, long = coordinates
+        lat, long, atm_group = message.text.removeprefix('/predict').strip().split(', ')
     except ValueError:
-        return await message.reply(f"Некорректные данные")
+        return await message.reply("Некорректные данные")
+    
+    if not atm_group in banks:
+        return await message.reply("Некорректные данные")
 
     async with aiohttp.ClientSession() as session:
-        async with session.post('https://atm-project.onrender.com/predict',
-                                json={'id_user': user_id, 'lat':lat, 'long':long}) as res:
+        async with session.post('https://atmapi-dahy.onrender.com/predict',
+                                json={'id_user': user_id, 'lat': lat, 'long': long, 'atm_group': atm_group}) as res:
             if res.status == 200:
                 prediction = await res.json()
                 await message.reply(f"{prediction['prediction']}")
             else:
-                await message.reply(f"Некорректные данные")
+                await message.reply("Некорректные данные")
 
 
-@dp.message_handler(commands=['predict_batch'])
-async def process_predict_batch_command(message: types.Message):
-    await PredictBatchSG.file.set()
+@dp.message(F.text, Command('predict_batch'))
+async def cmd_predict_batch(message: Message, state: FSMContext):
+    await state.set_state(PredictBatchSG.batch)
 
-    await message.answer("Введите координаты (каждая пара координат с новой строки)",
+    await message.answer("Введите координаты и наименование банка (информация о каждом банкомате с новой строки)",
                          reply_markup=cancel())
 
 
-@dp.message_handler(state=PredictBatchSG.file)
-async def process_send_batch(message: types.File, state: FSMContext):
-    user_id = int(message.from_id)
-    coords = list(map(float, re.split('[^\w.]+', message.text.strip())))
-    lats = coords[::2]
-    longs = coords[1::2]
-    n_rows = len(message.text.split('\n'))
-    if len(coords)/2 != n_rows:
-        return await message.reply(f"Некорректные данные")
+@dp.message(F.text, PredictBatchSG.batch)
+async def send_batch(message: Message, state: FSMContext):
+    user_id = int(message.from_user.id)
+    try:
+        rows = message.text.strip().split('\n')
+        data = [list(map(float, row.split(', ')[:2])) + [row.split(', ')[2]]  for row in rows]
+    except (ValueError, IndexError):
+        return await message.reply("Некорректные данные")
+    lats = [row[0] for row in data]
+    longs = [row[1] for row in data]
+    atm_groups = [row[2] for row in data]
 
+    for atm_group in atm_groups:
+        if not atm_group.upper() in banks:
+            return await message.reply("Некорректные данные")
 
     async with aiohttp.ClientSession() as session:
-        async with session.post('https://atm-project.onrender.com/predict_batch',
-                                json={'id_user': user_id, 'lat':lats, 'long':longs}) as res:
+        async with session.post('https://atmapi-dahy.onrender.com/predict_batch',
+                                json={'id_user': user_id, 'lat': lats, 'long': longs, 'atm_group': atm_groups}) as res:
             if res.status == 200:
                 predictions = await res.json()
-                msg = '\n'.join([f"({pred['lat']}, {pred['long']}): {pred['prediction']}" for pred in predictions])
-                await message.reply(msg)
-                await state.finish()
+                msg = '\n'.join([f"({pred['lat']}, {pred['long']}, {pred['atm_group']}): {pred['prediction']}" for pred in predictions])
+                await message.reply(msg, reply_markup=ReplyKeyboardRemove())
+                await state.clear()
             else:
-                await message.reply(f"Некорректные данные")
-    
-    
+                await message.reply("Некорректные данные")
+
+
+async def main():
+    await dp.start_polling(bot)
+
+
 if __name__ == '__main__':
-    
-    executor.start_polling(dp, skip_updates=False)
+    asyncio.run(main())
